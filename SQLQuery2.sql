99b8f7b73c6d3a3bcdec27696d1cc2882afa0df6
Toyosi-Expendables <147008531+Toyosi-Expendables@users.noreply.github.com>
2023-10-23 15:46:18 -0600

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,9 +1,9 @@
 select * from COUNTRIES
 select*from departments
-select * from employees
+select * from employees select*from departments
 select * from job_history
 select * from jobs
-select * from LOCATIONS 
+select * from LOCATIONS select*from departments
 select * from REGIONS
 select * From employees --order by salary desc
 --.) Display the minimum salary. 
@@ -36,8 +36,32 @@ select first_name, last_name,department_id from employees where department_id =
 (select department_id from employees where first_name = 'lisa' and last_name ='ozer')
 
 --10.) Display all the employees in the same department as Martha Sullivan and that make more than TJ Olson.
-
-NICELY DONE!
 select first_name, last_name,department_id, salary from employees where department_id =
 (select department_id from employees where first_name = 'Martha' and last_name ='Sullivan')
 and salary > (select salary from employees where first_name = 'tj' and last_name = 'olson')order by salary desc
+
+
+--11.) Display all the departments that exist in the departments table that are not in the employees’ table. Do not use a where clause.
+select department_id from departments
+except
+select distinct department_id from employees
+--12.) Display all the departments that exist in department tables that are also in the employees’ table. Do not use a where clause.
+select department_id from departments
+intersect
+select distinct department_id from employees
+
+--13.) Display all the departments name, street address, postal code, city, and state of each department. Use the departments and locations table for this query
+SELECT d.department_id, d. department_name, l.street_address, l.postal_code, l.city, l.state_province
+	from departments d join LOCATIONS l on D.location_id = l.location_id
+--14.) Display the first name and salary of all the employees in the accounting departments.
+select a.department_id,a.department_name,b.first_name,b.salary
+	from departments a join employees b on a.department_id = b.department_id where a.department_name = 'accounting'
+--15.) Display all the last name of all the employees whose department location id are 1700 and 1800.
+--16.) Display the phone number of all the employees in the Marketing department.
+select a.department_id,a.department_name,b.phone_number
+	from departments a join employees b on a.department_id = b. department_id where a.department_name = 'marketing'
+--17.) Display all the employees in the Shipping and Marketing departments who make more than 3100. 
+select a.depa
+--18). Write an SQL query to print the first three characters of FIRST_NAME from employee’s table.
+ --19.) Display all the employees who were hired before Tayler Fox.
+--20.) Display names and salary of the employees in executive department. 
